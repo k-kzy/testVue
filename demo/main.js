5baf5d2a237b42ab18b57cec4189d7f7a24f0dd5
k-kzy <rip.to.the.cat+github@gmail.com>
2020-05-10 15:41:24 +0900

    test up

--- demo/main.js
+++ demo/main.js
@@ -1,15 +1,32 @@
-new Vue({
-	el: '#app',
-	data: {
-		list:[]
-	},
-	watch: {
-		list: function() {
-			console.log('通常：', this.$refs.list.offsetHeight)
+// new Vue({
+//   el: '#app',
+//   data: {
+//     order: false,
+//     list: [
+//       { id: 1, name: 'りんご', price: 100 },
+//       { id: 2, name: 'ばなな', price: 200 },
+//       { id: 3, name: 'いちご', price: 300 }
+//     ]
+//   },
+//   computed: {
+//     sortedList: function() {
+//       return _.orderBy(this.list, 'price', this.order ? 'desc' : 'asc')
+//     }
+//   }
+// })
 
-			this.$nextTick(function() {
-				console.log('nextTick:', this.$refs.list.offsetHeight)
-			})
-		}
-	}
+Vue.component('my-circle', {
+  template: '<circle cx="80" cy="75" r="50" v-bind:fill="fill"/>',
+  props: { fill: String }
+})
+new Vue({
+  el: '#app',
+  data: {
+    toggle: false
+  },
+  computed: {
+    fill: function() {
+      return this.toggle ? 'lightpink' : 'skyblue'
+    }
+  }
 })
