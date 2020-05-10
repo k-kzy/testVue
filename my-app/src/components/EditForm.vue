944df0f79331f8de7be958a49d3141c1df71fdf0
k-kzy <rip.to.the.cat+github@gmail.com>
2020-05-10 23:02:19 +0900

    vuex demo up

---
+++ my-app/src/components/EditForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="edit-form">
+    <input type="text" :value="message" @input="doUpdate">
+  </div>
+</template>
+
+<script>
+import store from '@/store.js'
+export default {
+  name: 'EditForm',
+  computed: {
+    message () { return this.$store.getters.message }
+  },
+  methods: {
+    doUpdate (event) {
+      // input の値を持ってディスパッチ
+      // eslint-disable-next-line
+      store.dispatch ('doUpdate', event.target.value)
+    }
+  }
+}
+</script>
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,13 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
+import store from './store.js'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+// eslint-disable-next-line
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  store, // storeをローカルに登録
+  render: h => h(App)
 })
