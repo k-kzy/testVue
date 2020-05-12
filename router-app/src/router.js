b255a3a3b526c367b1beb755e64a5c46bd158bfe
窪内和也 <kubouchi@bita.jp>
2020-05-12 13:47:35 +0900

    update：router demo

--- router-app/src/router.js
+++ router-app/src/router.js
@@ -1,4 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+// ルート用のコンポーネントを読み込む
+import Home from '@/views/Home.vue'
+import Product from '@/views/Product.vue'
+
+// Vuexと同様で最初にプラグインとして登録
 Vue.use(VueRouter)
 
+// VueRouterインスタンスを生成する
+const router = new VueRouter({
+  // mode: 'history',
+  // URL のパスと紐づくコンポーネントをマッピング
+  routes: [
+    { path: '/', component: Home },
+    { path: '/product', component: Product }
+  ]
+})
+
+// 生成した VueRouter インスタンスをエクスポート
+export default router
