eb0b3db4992c3a09651d4e4f484568531d7ed3f6
窪内和也 <kubouchi@bita.jp>
2020-05-13 18:49:16 +0900

    update：router api

--- router-app/src/router.js
+++ router-app/src/router.js
@@ -10,7 +10,7 @@ import ProductList from '@/views/ProductList.vue'
 Vue.use(VueRouter)
 
 // VueRouterインスタンスを生成する
-const router = new VueRouter({
+export default new VueRouter({
   // mode: 'history',
   base: '/router-app/',
   // URL のパスと紐づくコンポーネントをマッピング
@@ -25,11 +25,9 @@ const router = new VueRouter({
     },
     {
       path: '/product/:id(\\d+)', // 「:id」がパラメータ 何が入ってもOK（数字のみマッチさせる正規表現）
-      component: Product
+      component: Product,
+      // 関数の場合第1引数として現在のルートオブジェクトが使用できる
+      props: route => ({ id: Number(route.params.id) })
     }
   ]
 })
-
-
-// 生成した VueRouter インスタンスをエクスポート
-export default router
